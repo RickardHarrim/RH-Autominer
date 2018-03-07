ec2295f9a0f7bbcb24c48a0b3a4ed41b6701b5eb
Rickard <37144658+RickardHarrim@users.noreply.github.com>
2018-03-07 20:57:50 +0100

    laucher
    This batch file laches the script.

---
+++ RH Autominer/laucher.bat
@@ -0,0 +1,6 @@
+cd "C:\Users\%username%\Documents\RH Autominer"
+timeout 1 >nul
+wscript.exe "C:\Users\%username%\Documents\RH Autominer\invisible.vbs"^
+	"C:\Users\%username%\Documents\RH Autominer\config.bat"
+	
+exit
